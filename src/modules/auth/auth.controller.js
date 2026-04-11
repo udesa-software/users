b52430f0a23675363bce9fbd7162d91ed44ad0db
delfinachavez <dchavezblasi@udesa.edu.ar>
2026-04-10 23:36:02 -0300

    pongo cookie secure siempre, no solo en prod para poder verlo bien en Postman

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,14 +1,12 @@
 const { authService } = require('./auth.service');
-const { env } = require('../../config/env');
 
 const REFRESH_COOKIE_NAME = 'refreshToken';
 
 function getRefreshCookieOptions() {
-  const isProduction = env.APP_URL.startsWith('https');
   return {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? 'None' : 'Lax',
+    secure: true,
+    sameSite: 'None',
     path: '/api/auth',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
   };
