44dc08437a60890d6014e900a5f8c3d7743e4c99
glewit <glewit@udesa.edu.ar>
2026-06-21 21:05:08 -0300

    h9 friends, denunciar usuario

--- src/modules/users/internal.routes.js
+++ src/modules/users/internal.routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const { internalController } = require('./internal.controller');
+const { authenticateInternal } = require('../../middlewares/authenticateInternal');
 
 const router = Router();
 
@@ -10,6 +11,9 @@ router.patch('/users/:id/privacy', internalController.updatePrivacy);
 router.get('/users/:id/preferences', internalController.getPreferences);
 router.post('/users/:id/suspend', internalController.suspendUser);
 router.post('/users/:id/unsuspend', internalController.unsuspendUser);
+// H9: solo estas dos rutas nuevas exigen el secreto interno (gap preexistente en el resto)
+router.post('/users/:id/flag-review', authenticateInternal, internalController.flagReview);
+router.post('/users/:id/resolve-review', authenticateInternal, internalController.resolveReview);
 router.get('/metrics', internalController.getMetrics);
 router.post('/users/online-status', internalController.getOnlineStatus);
 router.post('/users/candidates', internalController.getCandidates);
